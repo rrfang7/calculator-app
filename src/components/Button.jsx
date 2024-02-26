22b6fc72fedef41271e9f4556c7c532631b4283d
Ralph Fang <ralphrainierfang@yahoo.com>
2024-02-26 11:17:29 -0500

    Button Styling added and  Button.jsx functionality / classNames

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+const getStyleName = btn => {
+    const className = {
+        "=": "equals",
+        "x": "opt",
+        "-": "opt",
+        "+": "opt",
+        "/": "opt",
+    }
+    return className[btn]
+}
+
 function Button( { value }) {
   return (
-    <div className='button'>{value}</div>
+    <div className={`${getStyleName(value)} button`}>{value}</div>
   )
 }
 
