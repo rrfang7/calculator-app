cf4a00ed965c6f493200367ce4696993e3ba1f0e
Ralph Fang <ralphrainierfang@yahoo.com>
2024-02-26 22:04:06 -0500

    handleClickButton implemented on Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -21,22 +21,42 @@ function Button( { value }) {
     const commaClick = () => {
         setCalc ({
             ...calc, 
-            num: 99
-            // calc.num.toString().includes(".") ? calc.num + value: calc.num
-        })
+            num: !calc.num.toString().includes('.') ? calc.num + value : calc.num
+             
+        });
     }
     // User click C 
     const resetClick = () => {
         setCalc({ sign: "", num: 0, res: 0})
+    }
+    // User click numbers
+    const handleClickButton = () => {
+        const numberString = value.toString()
+
+        let numberValue;
+        if(numberString === "0" && calc.num === 0) {
+            numberValue = "0"
+        } else {
+            numberValue = Number(calc.num + numberString)
+        }
+
+        setCalc({
+            ...calc,
+            num: numberValue
+        })
     }
 
     const handleBtnClick = () => {
        
         const results = {
             ".": commaClick,
-            "C": resetClick
+            "C": resetClick,
         }
-        return results[value]
+        if(results[value]) {
+            return results[value]()
+        } else {
+            return handleClickButton()
+        }
     }
 
   return (
